f4b40e9c5839e59374479506131220debf605af9
supermanIT <53952001+supermanIT@users.noreply.github.com>
2020-05-09 19:22:55 +0800

    add test API

--- src/config/express.js
+++ src/config/express.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from "helmet";
+import router from "../routes";
 
 dotenv.config();
 
@@ -24,6 +25,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', router);
 
 export default app;
 
